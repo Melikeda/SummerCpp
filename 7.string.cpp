2434e65fbbf3c4b7d4d729bb1892774220dadce9
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-11 15:04:07 +0300

    Update 7.string.cpp

--- 7.string.cpp
+++ 7.string.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	string text = "ILOVEYOU";
-	cout << "The lenght of the text string is: " << text.length();
+	cout << "The lenght of the text string is: " << text.length(); //You can use "size()" if you want
 
 	return 0;
 }
