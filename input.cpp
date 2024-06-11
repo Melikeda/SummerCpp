a2924d12eb15a02a3cc6d5681c8790b80c8c76cb
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-11 14:37:33 +0300

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	int sum;
+
+	cout << "Please enter a number: ";
+	cin >> x;
+	cout << "Please enter an another number: ";
+	cin >> y;
+	sum = x + y;
+	cout << "Sum is: " << sum;
+
+	return 0;
+}
