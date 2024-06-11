9ce0a2a01a6709d98fe56fff9f84b8ded725065e
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-11 14:59:08 +0300

    Add files via upload

---
+++ 6.string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string firstName = "John";
+	string lastName = "Doe";
+	string fullName = firstName.append(lastName);  //you can also concatenate strings with the append() function
+	cout << fullName;
+	return 0;
+}
