399a7b31e3298adbf0eed40c0de27316ecd7ac40
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-11 15:02:23 +0300

    Add files via upload

---
+++ 7.string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string text = "ILOVEYOU";
+	cout << "The lenght of the text string is: " << text.length();
+
+	return 0;
+}
