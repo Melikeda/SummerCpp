ee3ce87df07879c2e0ab5fed0caae5369597d272
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-11 14:47:52 +0300

    Add files via upload

---
+++ 4.datatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int myNum = 5;
+	myNum = 10;
+	float myFloatNum = 2.52;
+	double myDoubleNum = 9.999;
+	char myChar = 'A';
+	bool myBoolean = 'True';
+	string myString = "So hot!";
+
+	cout << myNum << " " << myFloatNum << " " << myDoubleNum << " " << myChar << " " << myBoolean << "\n" << myString;
+
+	return 0;
+}
