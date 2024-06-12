ab1e6762dd5fbe210816966d17751aae249c846a
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-12 16:26:33 +0300

    Add files via upload

---
+++ 98.functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void myFunction(); //Function declaration
+
+int main() {
+	myFunction(); //Call function
+	return 0;
+}
+
+//Function defination
+void myFunction() {
+	cout << "Good night yastaa! Sweet dreams.";
+}
