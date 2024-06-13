ccf111ffeb48bdcdcb088095ebb83adf0faa9016
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-13 11:36:11 +0300

    Add files via upload

---
+++ 994.pointers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string food = "pizza";
+	string* ptr = &food;
+
+	cout << food << "\n";
+	cout << *ptr << "\n";
+	cout << ptr << "\n";
+	cout << &food << "\n\n";
+
+	*ptr = "Hamburger";
+
+	cout << food << "\n";
+	cout << *ptr << "\n";
+	cout << ptr << "\n";
+	cout << &food << "\n";
+	
+
+
+	
+	return 0;
+}
