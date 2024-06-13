c8b27f3c3c57f0504e9f6eef3bda8a6700eee22f
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-13 15:50:53 +0300

    Add files via upload

---
+++ 997.arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int myarray[4] = { 10,20,30,40 };
+	cout << sizeof(myarray) <<"\n";
+
+	string Myarray[3] = { "melike", "esma" "mehmet" };
+	cout << sizeof(Myarray);
+
+	return 0;
+}
