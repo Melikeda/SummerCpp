8605bc7dffb28bccae6d8816b2542a559b818f6a
Melike KÜLAHCI <m.edakulahci@gmail.com>
2024-06-14 15:02:32 +0300

    Add files via upload

---
+++ 9998.exceptions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    try {
+        int  age = 15;
+        if (age>=18) {
+            cout<< "Access granted - you are old enough.";
+        }
+        else {
+            throw(505);
+        }
+    }
+
+    catch(...){  //If you do not know the throw type used in the try block, you can use the "three dots" syntax (...) inside the catch block, which will handle any type of exception:
+        cout<<"Access denied - You must be at least 18 years old.\n";
+    }
+
+    return 0;
+}
